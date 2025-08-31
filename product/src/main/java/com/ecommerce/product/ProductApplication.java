6890c132e9534c870a849fa0c46174dc27e30090
Thái Văn Lâm <thaivanlam373@gmail.com>
2025-08-31 11:40:01 +0700

    123. Finalizing Changes Going Forward

--- product/src/main/java/com/ecommerce/product/ProductApplication.java
+++ product/src/main/java/com/ecommerce/product/ProductApplication.java
@@ -3,11 +3,14 @@ package com.ecommerce.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ProductApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductApplication.class, args);
-	}
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+        SpringApplication.run(ProductApplication.class, args);
+    }
 
 }
