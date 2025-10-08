98eefb94c385c817953f6f0c610421fd0c065563
Thái Văn Lâm <thaivanlam373@gmail.com>
2025-10-08 10:19:49 +0700

    177. Using @LoadBalanced with RestClient – The Modern Approach

--- addition/interservice/consumer/src/main/java/com/demo/consumer/restclient/RestClientConfig.java
+++ addition/interservice/consumer/src/main/java/com/demo/consumer/restclient/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.demo.consumer.restclient;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -7,7 +8,13 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
     @Bean
+    @LoadBalanced
+    public RestClient.Builder loadbalancedRestClientBuilder(){
+        return RestClient.builder();
+    }
+
+    @Bean
     public RestClient restClient(RestClient.Builder builder){
-        return builder.baseUrl("http://localhost:8081").build();
+        return builder.baseUrl("http://provider").build();
     }
 }
